882a2d94e89107f092019b9b6c62393792345239
kazi331 <kazisharifulislam52@gmail.com>
2025-08-06 15:34:42 +0600

    shadcn added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <div>
-      <h2>React Drag N Drop</h2>
+      <h2 className="text-center m-2">React Drag N Drop</h2>
       <button className="text-lg p-2 text-rose-400 capitalize bg-rose-400/20 rounded-lg px-10 m-2 active:bg-rose-500/20 cursor-pointer">button</button>
     </div>
   )
