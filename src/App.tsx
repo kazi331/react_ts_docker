940a10d9e485d665f2d3a19aca3583da9d93c20a
kazi331 <kazisharifulislam52@gmail.com>
2025-08-06 16:07:19 +0600

    path alias issue resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Button } from "./components/ui/button"
+import { Card } from "./components/ui/card"
+
 function App() {
 
   return (
     <div>
       <h2 className="text-center m-2">React Drag N Drop</h2>
-      <button className="text-lg p-2 text-rose-400 capitalize bg-rose-400/20 rounded-lg px-10 m-2 active:bg-rose-500/20 cursor-pointer">button</button>
+      <Button>Button</Button>
+      <Card>Card</Card>
     </div>
   )
 }
